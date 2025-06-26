e24ecface7e916f899a33f4babaefa849666db06
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-06-26 19:44:11 -0300

    fix: otra de emoji y tipo con minuscula

--- api/obtenerTipos.js
+++ api/obtenerTipos.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwUfUOsRXavh1G_rPeu-8jXmgoTOAp7KW43uX0jIjZlUFvTYivMz5gH5lPUhV_l1Y6JGA/exec');
+      const response = await fetch('https://script.google.com/macros/s/AKfycbwUfUOsRXavh1G_rPeu-8jXmgoTOAp7KW43uX0jIjZlUFvTYivMz5gH5lPUhV_l1Y6JGA/exec');
     
     if (!response.ok) {
       throw new Error(`Error al obtener datos: ${response.status}`);
