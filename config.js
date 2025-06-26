4ad392a2d7aac9b6345529981cad3eb3ec380941
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-06-26 13:04:21 -0300

    cambios para trabajar en gs de prueba

--- config.js
+++ config.js
@@ -1,6 +1,2 @@
-const config = {
-  endpoint: 'https://script.google.com/macros/s/AKfycbwUfUOsRXavh1G_rPeu-8jXmgoTOAp7KW43uX0jIjZlUFvTYivMz5gH5lPUhV_l1Y6JGA/exec'
-};
-
-export default config;
-
+//config.js
+window.ENDPOINT_URL = 'https://script.google.com/macros/s/AKfycbwUfUOsRXavh1G_rPeu-8jXmgoTOAp7KW43uX0jIjZlUFvTYivMz5gH5lPUhV_l1Y6JGA/exec';
