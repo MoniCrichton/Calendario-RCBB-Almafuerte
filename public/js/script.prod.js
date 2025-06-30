23bfad6ec8d3cb5cd61c44c6ecd3355f123c1f7a
MoniCrichton <email@example.comcodeando3@gmail.com>
2025-06-29 23:15:13 -0300

    corregidos los arvhicos derutas

--- public/js/script.prod.js
+++ public/js/script.prod.js
@@ -1,2 +1,15 @@
-window.SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwUfUOsRXavh1G_rPeu-8jXmgoTOAp7KW43uX0jIjZlUFvTYivMz5gH5lPUhV_l1Y6JGA/exec";
+fetch("https://opensheet.vercel.app/1S7ZFwciFjQ11oScRN9cA9xVVtuZUR-HWmMVO3HWAkg4/Consignas")
+  .then(res => res.json())
+  .then(data => console.log("✅ PROD: Consignas", data));
 
+fetch("https://opensheet.vercel.app/1S7ZFwciFjQ11oScRN9cA9xVVtuZUR-HWmMVO3HWAkg4/Feriados")
+  .then(res => res.json())
+  .then(data => console.log("✅ PROD: Feriados", data));
+
+fetch("https://opensheet.vercel.app/1S7ZFwciFjQ11oScRN9cA9xVVtuZUR-HWmMVO3HWAkg4/Cumples")
+  .then(res => res.json())
+  .then(data => console.log("✅ PROD: Cumples", data));
+
+fetch("https://script.google.com/macros/s/AKfycbz4IwyrKpNxD-fq3lt5M1E-hNMVovV3Raq58nQ67EyYO6ub8gHyo2FbOnX7DOUcN5wG/exec")
+  .then(res => res.json())
+  .then(data => console.log("✅ PROD: Eventos", data));
